b967630ea1dec803ee51bf0a19eb5798a3d36222
Dan <68809853+Dansmith-rgb@users.noreply.github.com>
2020-11-26 16:35:20 +0000

    Add files via upload

--- libary.py
+++ libary.py
@@ -43,10 +43,5 @@ btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
 btn2 = Button(root,text="Delete Book",bg='black', fg='white', command=db.deleteBook)
 btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
     
-btn3 = Button(root,text="Issue Book to Student",bg='black', fg='white')
-btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
-    
-btn4 = Button(root,text="Return Book",bg='black', fg='white')
-btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
-    
 root.mainloop()
+
